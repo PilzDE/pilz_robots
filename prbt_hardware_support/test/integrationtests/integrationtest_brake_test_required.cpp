dcc5ae1d17f89fde918fb6f7e814e33634196a64
imartini <i.martini@pilz.de>
2019-05-07 09:28:58 +0200

    Announce braketest for SafeBrakeControl (SBC)
    A braketest requirement by the safety controller is
    announce via ROS topic. The modbus API was extended with
    a flag indicating a pending braketest request which is translated
    into a boolean ROS message.

---
+++ prbt_hardware_support/test/integrationtests/integrationtest_brake_test_required.cpp
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2019 Pilz GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <thread>
+#include <memory>
+#include <chrono>
+#include <vector>
+#include <functional>
+#include <condition_variable>
+#include <atomic>
+#include <string>
+
+#include <ros/ros.h>
+#include <std_srvs/SetBool.h>
+#include <std_srvs/Trigger.h>
+#include <std_msgs/Bool.h>
+
+#include <modbus/modbus.h>
+
+#include <prbt_hardware_support/ModbusMsgInStamped.h>
+#include <prbt_hardware_support/pilz_modbus_server_mock.h>
+#include <prbt_hardware_support/pilz_modbus_read_client.h>
+
+#include <prbt_hardware_support/ros_test_helper.h>
+
+#include <pilz_testutils/async_test.h>
+
+namespace prbt_hardware_support
+{
+
+using ::testing::_;
+using ::testing::InSequence;
+using ::testing::Invoke;
+using ::testing::InvokeWithoutArgs;
+
+static constexpr uint16_t MODBUS_API_VERSION_VALUE {2};
+static const std::string TOPIC_BRAKE_TEST_REQUIRED = "/prbt/brake_test_required";
+static constexpr int DEFAULT_QUEUE_SIZE_BRAKE_TEST {1};
+
+template<class T>
+static void initalizeAndRun(T& obj, const char *ip, unsigned int port)
+{
+  if ( !obj.init(ip, port) )
+  {
+    ROS_ERROR("Initialization failed.");
+    return;
+  }
+  ROS_INFO_STREAM("Starting Server on " << ip << ":" << port);
+
+  obj.run();
+}
+
+// Define matcher for the brake test messages
+MATCHER(InformsAboutRequired, "") { return arg.data; }
+MATCHER(InformsAboutNotRequired, "") { return !arg.data; }
+
+/**
+ * @brief BrakeTestRequiredIntegrationTest checks if the chain
+ * ModbusServerMock -> ModbusReadClient -> ModbusBrakeTestAnnouncer functions properly
+ *
+ * @note the test is derived from testing::AsyncTest which allows the asynchronous processes to re-sync
+ */
+class BrakeTestRequiredIntegrationTest : public testing::Test, public testing::AsyncTest
+{
+protected:
+  BrakeTestRequiredIntegrationTest();
+  /**
+   * @brief Start Modbus-server in separate thread. Make sure that the nodes are up.
+   */
+
+  MOCK_METHOD1(brake_test_announcer_callback, void(std_msgs::Bool msg));
+
+protected:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_priv_{"~"};
+  ros::Subscriber brake_test_topic_sub_;
+};
+
+BrakeTestRequiredIntegrationTest::BrakeTestRequiredIntegrationTest()
+{
+  brake_test_topic_sub_ = nh_.subscribe<std_msgs::Bool>(TOPIC_BRAKE_TEST_REQUIRED,
+                                                           DEFAULT_QUEUE_SIZE_BRAKE_TEST,
+                                                           &BrakeTestRequiredIntegrationTest::brake_test_announcer_callback,
+                                                           this);
+}
+
+/**
+ * @brief Send data via ModbusServerMock -> ModbusReadClient -> ModbusBrakeTestAnnouncer connection
+ * and check that the expected message arrives on the brake test topic.
+ *
+ * @note Due to the asynchronicity of the test each step of the sequence passed successful
+ *       allows the next step to be taken. See testing::AsyncTest for details.
+ *
+ * Test Sequence:
+ *    0. Start Modbus-server in separate thread. Make sure that the nodes are up.
+ *    1. Send a brake test required message with the correct API version.
+ *    2. Send a brake test required message with the correct API version (change another but irelevant register entry).
+ *    3. Send a brake test not-required message with the correct API version.
+ *    4. Terminate ModbusServerMock.
+ *
+ * Expected Results:
+ *    0. -
+ *    1. A message informing about the required brake test arrives on the brake test topic.
+ *    2. No message arrives on the brake test topic.
+ *    3. A message informing about the not-required brake test arrives on the brake test topic.
+ *    4. No message arrives on the brake test topic.
+ */
+TEST_F(BrakeTestRequiredIntegrationTest, testBrakeTestAnnouncement)
+{
+  EXPECT_GE(std::thread::hardware_concurrency(), 2) << "Hardware does not support enough threads";
+
+  /**********
+   * Setup *
+   **********/
+  std::string ip;
+  int port;
+  ASSERT_TRUE(nh_priv_.getParam("modbus_server_ip", ip));
+  ASSERT_TRUE(nh_priv_.getParam("modbus_server_port", port));
+
+  int modbus_register_size, num_registers_to_read, index_of_first_register_to_read;
+  ASSERT_TRUE(nh_priv_.getParam("modbus_register_size", modbus_register_size));
+  ASSERT_TRUE(nh_priv_.getParam("num_registers_to_read", num_registers_to_read));
+  ASSERT_TRUE(nh_priv_.getParam("index_of_first_register_to_read", index_of_first_register_to_read));
+
+  /**********
+   * Step 0 *
+   **********/
+  prbt_hardware_support::PilzModbusServerMock modbus_server(static_cast<unsigned int>(modbus_register_size));
+
+  std::thread modbus_server_thread( &initalizeAndRun<prbt_hardware_support::PilzModbusServerMock>,
+                                    std::ref(modbus_server), ip.c_str(), static_cast<unsigned int>(port) );
+
+  waitForNode("/pilz_modbus_read_client_node");
+  waitForNode("/modbus_brake_test_announcer_node");
+
+  // We expect:
+  {
+    InSequence dummy;
+
+    EXPECT_CALL(*this, brake_test_announcer_callback(InformsAboutRequired()))
+      .Times(1)
+      .WillOnce(ACTION_OPEN_BARRIER_VOID("brake_test_announcer_required"));
+
+    EXPECT_CALL(*this, brake_test_announcer_callback(InformsAboutNotRequired()))
+      .Times(1)
+      .WillOnce(ACTION_OPEN_BARRIER_VOID("brake_test_announcer_not_required"));
+  }
+
+  /**********
+   * Step 1 *
+   **********/
+  std::vector<uint16_t> required_holding_register{MODBUS_API_VERSION_VALUE, 0, 0, 0, 1};
+  modbus_server.setHoldingRegister(required_holding_register, index_of_first_register_to_read);
+
+  BARRIER("brake_test_announcer_required");
+
+  /**********
+   * Step 2 *
+   **********/
+  std::vector<uint16_t> required_holding_register_changed{MODBUS_API_VERSION_VALUE, 1, 0, 0, 1};
+  modbus_server.setHoldingRegister(required_holding_register_changed, index_of_first_register_to_read);
+
+  /**********
+   * Step 3 *
+   **********/
+  std::vector<uint16_t> not_required_holding_register{MODBUS_API_VERSION_VALUE, 0, 0, 0, 0};
+  modbus_server.setHoldingRegister(not_required_holding_register, index_of_first_register_to_read);
+
+  BARRIER("brake_test_announcer_not_required");
+
+  /**********
+   * Step 4 *
+   **********/
+  modbus_server.terminate();
+  modbus_server_thread.join();
+
+  // Cannot use BARRIER here. Sleep prevents early termination.
+  sleep(2.0);
+}
+
+} // namespace prbt_hardware_support
+
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "integrationtest_brake_test_required");
+  ros::NodeHandle nh;
+
+  ros::AsyncSpinner spinner_{1};
+  spinner_.start();
+
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ prbt_hardware_support/test/unittests/unittest_brake_test_filter.cpp
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2019 Pilz GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <memory>
+#include <boost/bind.hpp>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <ros/ros.h>
+
+#include <prbt_hardware_support/brake_test_filter.h>
+#include <prbt_hardware_support/modbus_msg_brake_test_wrapper.h>
+#include <prbt_hardware_support/modbus_msg_in_utils.h>
+#include <prbt_hardware_support/ModbusMsgInStamped.h>
+#include <message_filters/pass_through.h>
+#include <message_filters/subscriber.h>
+
+namespace mf = message_filters;
+
+template class message_filters::BrakeTestFilter<prbt_hardware_support::ModbusMsgInStamped>;
+
+namespace brake_test_filter_test
+{
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::Throw;
+using namespace prbt_hardware_support;
+
+static constexpr ModbusApiSpec test_api_spec(696, 737);
+
+class CallbackReceiver
+{
+public:
+  MOCK_METHOD1(modbusInMsgCallback, void(ModbusMsgInStampedConstPtr msg));
+};
+
+class TestPublisher : public ros::Publisher
+{
+public:
+  TestPublisher(const Publisher &rhs) : ros::Publisher::Publisher(rhs)
+  {
+  }
+
+  template <typename M>
+  void publishAndSpin(const boost::shared_ptr<M> &message) const
+  {
+    this->publish(message);
+    ros::spinOnce();
+  }
+};
+
+/**
+		 * @brief BrakeTestFilterTest tests the BrakeTestFilter class
+		 */
+class BrakeTestFilterTest : public testing::Test
+{
+public:
+  void SetUp() override;
+
+  void TearDown() override;
+
+  std::shared_ptr<CallbackReceiver> callback_receiver_;
+};
+
+void BrakeTestFilterTest::SetUp()
+{
+  this->callback_receiver_.reset(new CallbackReceiver());
+}
+
+void BrakeTestFilterTest::TearDown()
+{
+  this->callback_receiver_ = nullptr;
+}
+
+ModbusMsgInStampedPtr createDefaultBrakeTestModbusMsg(bool brake_test_required)
+{
+  uint32_t first_index_to_read{test_api_spec.version_register_};
+  uint32_t last_index_to_read{test_api_spec.braketest_register_};
+  static int msg_time_counter{1};
+  std::vector<uint16_t> tab_reg(last_index_to_read - first_index_to_read + 1);
+  tab_reg[0] = 1;
+  tab_reg[last_index_to_read - first_index_to_read] = brake_test_required;
+  ModbusMsgInStampedPtr msg{createDefaultModbusMsgIn(first_index_to_read, tab_reg)};
+  msg->header.stamp = ros::Time(msg_time_counter++);
+  return msg;
+}
+
+/**
+		 * @brief Tests that the filter properly only passes message to the callback that have a change in brake test state
+		 *        when starting with state true.
+		 */
+TEST_F(BrakeTestFilterTest, testBrakeTestFilteringThroughSubscriberFromTrue)
+{
+  std::string test_topic_name = "/test_topic";
+
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<ModbusMsgInStamped>(test_topic_name, 1);
+  TestPublisher test_pub(pub);
+  message_filters::Subscriber<ModbusMsgInStamped> modbus_sub(nh, test_topic_name, 1);
+
+  mf::BrakeTestFilter<ModbusMsgInStamped> brake_test_filter(modbus_sub, test_api_spec);
+
+  // Register to modbus callback
+  brake_test_filter.registerCallback(
+      boost::bind(&CallbackReceiver::modbusInMsgCallback, this->callback_receiver_, _1));
+
+  // Multiple messages will be send, only 3 have changed brake test state
+  EXPECT_CALL(*(this->callback_receiver_.get()), modbusInMsgCallback(_)).Times(3);
+
+  ModbusMsgInStampedPtr msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 1
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 2
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 3
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+}
+
+/**
+		 * @brief Tests that the filter properly only passes message to the callback that have a change in brake test state
+		 *        when starting with state false.
+		 */
+TEST_F(BrakeTestFilterTest, testBrakeTestFilteringThroughSubscriberFromFalse)
+{
+  std::string test_topic_name = "/test_topic";
+
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<ModbusMsgInStamped>(test_topic_name, 1);
+  TestPublisher test_pub(pub);
+  message_filters::Subscriber<ModbusMsgInStamped> modbus_sub(nh, test_topic_name, 1);
+
+  mf::BrakeTestFilter<ModbusMsgInStamped> brake_test_filter(modbus_sub, test_api_spec);
+
+  // Register to modbus callback
+  brake_test_filter.registerCallback(
+      boost::bind(&CallbackReceiver::modbusInMsgCallback, this->callback_receiver_, _1));
+
+  // Multiple messages will be send, only 3 have changed brake test state
+  EXPECT_CALL(*(this->callback_receiver_.get()), modbusInMsgCallback(_)).Times(3);
+
+  ModbusMsgInStampedPtr msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 1
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 2
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+  msg = createDefaultBrakeTestModbusMsg(true);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should trigger - CALL 3
+  msg = createDefaultBrakeTestModbusMsg(false);
+  test_pub.publishAndSpin(msg); // Should not trigger as state has not changed
+}
+
+} // namespace brake_test_filter_test
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "unittest_brake_test_filter");
+  testing::InitGoogleTest(&argc, argv);
+  ros::Time::init(); // Needed on message construction
+  return RUN_ALL_TESTS();
+}
