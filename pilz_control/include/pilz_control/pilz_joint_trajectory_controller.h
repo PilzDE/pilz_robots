555756b375e4b96c9578496681c5ae0de7d51708
imartini <i.martini@pilz.de>
2019-07-09 13:21:05 +0200

    Use boost::shared_ptr in joint traj controller

--- pilz_control/include/pilz_control/pilz_joint_trajectory_controller.h
+++ pilz_control/include/pilz_control/pilz_joint_trajectory_controller.h
@@ -19,6 +19,8 @@
 
 #include <mutex>
 
+#include <boost/shared_ptr.hpp>
+
 #include <std_srvs/Trigger.h>
 
 #include <joint_trajectory_controller/joint_trajectory_controller.h>
@@ -44,7 +46,7 @@ class PilzJointTrajectoryController
     typedef joint_trajectory_controller::JointTrajectorySegment<SegmentImpl> Segment;
     typedef std::vector<Segment> TrajectoryPerJoint;
     typedef std::vector<TrajectoryPerJoint> Trajectory;
-    typedef std::shared_ptr<Trajectory> TrajectoryPtr;
+    typedef boost::shared_ptr<Trajectory> TrajectoryPtr;
 
     PilzJointTrajectoryController();
 
