6789580eafcb0d2bc264f780be90c7a328b3e09d
imartini <i.martini@pilz.de>
2019-07-09 10:06:04 +0200

    plan unittest sto executor

--- prbt_hardware_support/test/unittests/unittest_sto_executor.cpp
+++ prbt_hardware_support/test/unittests/unittest_sto_executor.cpp
@@ -16,28 +16,148 @@
  */
 
 #include <functional>
+#include <memory>
 
 #include <ros/ros.h>
+#include <std_srvs/Trigger.h>
 
 #include <prbt_hardware_support/sto_executor.h>
 #include <prbt_hardware_support/service_client_mock.h>
 
-TEST(STOExecutorTest, testDummy)
+namespace prbt_hardware_support_tests
 {
-  using namespace prbt_hardware_support_tests;
 
+using namespace prbt_hardware_support;
+
+typedef ServiceClientMockFactory<std_srvs::Trigger> MockFactory;
+typedef STOExecutorTemplated<ServiceClientMock<std_srvs::Trigger>> STOExecutor;
+
+/**
+ * @brief Test D0 destructor
+ *
+ * Increases function coverage
+ */
+TEST(STOExecutorTest, testD0estructor)
+{
+  MockFactory factory;
+  std::shared_ptr<STOExecutor> sto_executor {new STOExecutor(std::bind(&MockFactory::create, &factory,
+                                                                       std::placeholders::_1))};
+}
+
+/**
+ * @brief destructor ends run
+ */
+
+/**
+ * @brief terminate ends run
+ */
+
+/**
+ * @brief enable
+ *
+ * Test Sequence:
+ *  1. update(true)
+ *
+ * Expected Results:
+ *  1. recover, unhold
+ */
+
+/**
+ * @brief disable plus recover
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during unhold, update(true) during halt
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, halt, recover, unhold
+ */
+
+/**
+ * @brief spam enable
+ *
+ * Test Sequence:
+ *  1. update(true) periodically
+ *
+ * Expected Results:
+ *  1. recover, unhold
+ */
+
+/**
+ * @brief spam disable plus recover
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during unhold and periodically, update(true)
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, halt, recover, unhold
+ */
+
+/**
+ * @brief skip hold
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during recover, update(true) during halt
+ *
+ * Expected Results:
+ *  1. recover, halt, recover, unhold
+ */
+
+/**
+ * @brief skip halt
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during unhold, update(true) during hold, update(false) during unhold
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, unhold, hold, halt
+ */
+
+/**
+ * @brief recover fail
+ *
+ * Test Sequence:
+ *  1. update(true), recover fails, update(false), update(true)
+ *
+ * Expected Results:
+ *  1. recover, halt, recover, unhold
+ */
+
+/**
+ * @brief unhold fail
+ *
+ * Test Sequence:
+ *  1. update(true), unhold fails, update(false), update(true) during halt
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, halt, recover, unhold
+ */
+
+/**
+ * @brief hold fail
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during unhold, hold fails
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, halt
+ */
+
+/**
+ * @brief halt fail
+ *
+ * Test Sequence:
+ *  1. update(true), update(false) during unhold, halt fails
+ *
+ * Expected Results:
+ *  1. recover, unhold, hold, halt
+ */
+
+}  // namespace prbt_hardware_support_tests
+
+int main(int argc, char **argv){
   // for (limited) ros::Time functionality, no ROS communication
   ros::Time::init();
 
-  typedef ServiceClientMockFactory<std_srvs::Trigger> MockFactory;
-  typedef prbt_hardware_support::STOExecutorTemplated<ServiceClientMock<std_srvs::Trigger>> STOExecutor;
-
-  MockFactory factory;
-
-  STOExecutor sto_executor{std::bind(&MockFactory::create, &factory, std::placeholders::_1)};
-}
-
-int main(int argc, char **argv){
   testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
 }
