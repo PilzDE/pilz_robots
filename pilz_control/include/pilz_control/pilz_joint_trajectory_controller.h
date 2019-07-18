deea4e1f513a978396292701ca208baf4024f64d
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2019-07-18 11:33:35 +0200

    Make hold call of controller invariant (cherry-pick ad8dd)
    * The second call to hold should return immediatly as
      there is nothing todo. Helps solving #151

--- pilz_control/include/pilz_control/pilz_joint_trajectory_controller.h
+++ pilz_control/include/pilz_control/pilz_joint_trajectory_controller.h
@@ -28,6 +28,12 @@
 namespace pilz_joint_trajectory_controller
 {
 
+enum class Mode
+{
+  HOLD,
+  UNHOLD
+};
+
 /**
  * @class PilzJointTrajectoryController
  * @brief Specialized controller that can be triggered by a service to
@@ -110,13 +116,13 @@ class PilzJointTrajectoryController
     void triggerMovementToHoldPosition();
 
   private:
-    std::function<bool(const JointTrajectoryConstPtr&, RealtimeGoalHandlePtr, std::string*)> active_update_strategy_;
-
     ros::ServiceServer hold_position_service;
     ros::ServiceServer unhold_position_service;
     ros::ServiceServer is_executing_service_;
 
     std_srvs::TriggerRequest last_request_;
+
+    Mode active_mode_ {Mode::HOLD};
 
     /**
      * @brief Synchronizes hold/unhold and update trajectory function to avoid
