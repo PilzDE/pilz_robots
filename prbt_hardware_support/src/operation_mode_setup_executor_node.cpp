9ecf0f5973602a71fcd1b5691ad485a16e498a9e
hslusarek <h.slusarek@pilz.de>
2019-09-12 14:57:05 +0200

    Add tests for service calls in operation_mode_setup_executor_node

--- prbt_hardware_support/src/operation_mode_setup_executor_node.cpp
+++ prbt_hardware_support/src/operation_mode_setup_executor_node.cpp
@@ -26,6 +26,7 @@
 #include <prbt_hardware_support/GetOperationMode.h>
 #include <prbt_hardware_support/get_operation_mode_func_decl.h>
 #include <prbt_hardware_support/get_param.h>
+#include <prbt_hardware_support/operation_mode_setup_executor_node_service_calls.h>
 
 static const std::string SET_SPEED_LIMIT_SERVICE{"set_speed_limit"};
 static const std::string OPERATION_MODE_SERVICE{"/prbt/get_operation_mode"};
@@ -36,37 +37,9 @@ static const std::string PARAM_SPEED_LIMIT_AUTO_STR {"speed_limit_automatic"};
 
 static constexpr uint32_t DEFAULT_QUEUE_SIZE {10} ;
 
-// LCOV_EXCL_START
-namespace prbt_hardware_support
-{
-
-bool setSpeedLimitSrv(ros::ServiceClient& srv_client, const double& speed_limit)
-{
-  SetSpeedLimit srv_msg;
-  srv_msg.request.speed_limit = speed_limit;
-  bool call_success = srv_client.call(srv_msg);
-  if (!call_success)
-  {
-    ROS_ERROR_STREAM("No success calling service: " << srv_client.getService());
-  }
-  return call_success;
-}
-
-OperationModes getOperationMode(ros::ServiceClient& srv_client)
-{
-  GetOperationMode srv_msg;
-  bool call_success = srv_client.call(srv_msg);
-  if (!call_success)
-  {
-    ROS_ERROR_STREAM("No success calling service: " << srv_client.getService());
-  }
-  return srv_msg.response.mode;
-}
-
-} // namespace prbt_hardware_support
-
 using namespace prbt_hardware_support;
 
+// LCOV_EXCL_START
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "operation_mode_setup_executor");
@@ -75,11 +48,13 @@ int main(int argc, char **argv)
   using std::placeholders::_1;
   waitForService(SET_SPEED_LIMIT_SERVICE);
   ros::ServiceClient speed_limit_srv = nh.serviceClient<SetSpeedLimit>(SET_SPEED_LIMIT_SERVICE);
-  SetSpeedLimitFunc set_speed_limit_func = std::bind(setSpeedLimitSrv, speed_limit_srv, _1);
+  SetSpeedLimitFunc set_speed_limit_func = std::bind(setSpeedLimitSrv<ros::ServiceClient>,
+                                                     speed_limit_srv, _1);
 
   waitForService(OPERATION_MODE_SERVICE);
   ros::ServiceClient op_mode_srv = nh.serviceClient<GetOperationMode>(OPERATION_MODE_SERVICE);
-  GetOpModeFunc get_op_mode_func = std::bind(getOperationMode, op_mode_srv);
+  GetOpModeFunc get_op_mode_func = std::bind(getOperationMode<ros::ServiceClient>,
+                                             op_mode_srv);
 
   double speed_limit_t1 {0};
   double speed_limit_auto {0};
