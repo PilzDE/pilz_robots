b24cdb126082af7530b147db5976ad8da93c3f8a
Immanuel Martini <i.martini@pilz.de>
2019-10-08 08:07:15 +0200

    Add tests with CMakeLists argument

--- prbt_hardware_support/test/integrationtests/integrationtest_stop1_service_missing.cpp
+++ prbt_hardware_support/test/integrationtests/integrationtest_stop1_service_missing.cpp
@@ -23,7 +23,8 @@
 
 #include <ros/ros.h>
 
-#include <prbt_hardware_support/pilz_manipulator_mock.h>
+#include <std_srvs/Trigger.h>
+
 #include <prbt_hardware_support/ros_test_helper.h>
 
 namespace prbt_hardware_support
@@ -38,6 +39,52 @@ static const std::string OMIT_SERVICE_PARAM_NAME {"omit_service"};
 static const std::string STOP1_EXECUTOR_NODE_NAME {"/stop1_executor_node"};
 
 static constexpr double WAIT_FOR_NODE_SLEEPTIME_S {5.0};
+
+bool triggerCallbackDummy(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &resp)
+{
+  return true;
+}
+
+class Stop1ServiceMissingIntegrationTest : public ::testing::Test
+{
+public:
+  void SetUp() override;
+
+protected:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_priv_ {"~"};
+
+  ros::ServiceServer hold_server_;
+  ros::ServiceServer unhold_server_;
+  ros::ServiceServer halt_server_;
+  ros::ServiceServer recover_server_;
+
+  std::function<void()> omit_advertise_func_;
+  std::map<std::string, std::function<void()>> advertise_funcs_;
+};
+
+void Stop1ServiceMissingIntegrationTest::SetUp()
+{
+  ROS_DEBUG("SetUp()");
+
+  advertise_funcs_[RECOVER_SERVICE_NAME] = [this](){
+    this->recover_server_ = this->nh_.advertiseService(RECOVER_SERVICE_NAME, triggerCallbackDummy); };
+  advertise_funcs_[HALT_SERVICE_NAME] = [this](){
+    this->halt_server_ = this->nh_.advertiseService(HALT_SERVICE_NAME, triggerCallbackDummy); };
+  advertise_funcs_[UNHOLD_SERVICE_NAME] = [this](){
+    this->unhold_server_ = this->nh_.advertiseService(UNHOLD_SERVICE_NAME, triggerCallbackDummy); };
+  advertise_funcs_[HOLD_SERVICE_NAME] = [this](){
+    this->hold_server_ = this->nh_.advertiseService(HOLD_SERVICE_NAME, triggerCallbackDummy); };
+
+  std::string omit_service;
+  ASSERT_TRUE(nh_priv_.getParam(OMIT_SERVICE_PARAM_NAME, omit_service));
+
+  const auto &it = advertise_funcs_.find(omit_service);
+  ASSERT_TRUE(it != advertise_funcs_.end()) << "Invalid service name: " << omit_service;
+
+  omit_advertise_func_ = it->second;
+  advertise_funcs_.erase(it);
+}
 
 /**
  * @brief Test that the startup of the stop1_executor is not complete if a service is missing.
@@ -54,51 +101,15 @@ static constexpr double WAIT_FOR_NODE_SLEEPTIME_S {5.0};
  *    1. Stop1 executor node does not come up
  *    2. Stop1 executor node does complete startup
  */
-TEST(Stop1StartupIntegrationTest, testMissingService)
+TEST_F(Stop1ServiceMissingIntegrationTest, testMissingService)
 {
-  /**********
-   * Setup *
-   **********/
-  ROS_DEBUG("Test Setup");
-
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_priv {"~"};
-  std::shared_ptr<ManipulatorMock> manipulator {std::make_shared<ManipulatorMock>()};
-
-  std::map<std::string, std::function<void()>> advertise_funcs;
-  advertise_funcs[RECOVER_SERVICE_NAME] = std::bind(&ManipulatorMock::advertiseRecoverService,
-                                                   manipulator,
-                                                   nh,
-                                                   RECOVER_SERVICE_NAME);
-  advertise_funcs[HALT_SERVICE_NAME] = std::bind(&ManipulatorMock::advertiseHaltService,
-                                                manipulator,
-                                                nh,
-                                                HALT_SERVICE_NAME);
-  advertise_funcs[HOLD_SERVICE_NAME] = std::bind(&ManipulatorMock::advertiseHoldService,
-                                                manipulator,
-                                                nh,
-                                                HOLD_SERVICE_NAME);
-  advertise_funcs[UNHOLD_SERVICE_NAME] = std::bind(&ManipulatorMock::advertiseUnholdService,
-                                                  manipulator,
-                                                  nh,
-                                                  UNHOLD_SERVICE_NAME);
-
-  std::string omit_service;
-  ASSERT_TRUE(nh_priv.getParam(OMIT_SERVICE_PARAM_NAME, omit_service));
-
-  const auto &it = advertise_funcs.find(omit_service);
-  ASSERT_TRUE(it != advertise_funcs.end());
-
-  const auto omit_advertise_func = it->second;
-  advertise_funcs.erase(it);
-
   /**********
    * Step 1 *
    **********/
   ROS_DEBUG("Step 1");
 
-  std::for_each(advertise_funcs.begin(),
-                advertise_funcs.end(),
+  std::for_each(advertise_funcs_.begin(),
+                advertise_funcs_.end(),
                 [](const std::pair<std::string, std::function<void()>> &el){ el.second(); });
 
   ros::Duration(WAIT_FOR_NODE_SLEEPTIME_S).sleep();
@@ -109,7 +120,7 @@ TEST(Stop1StartupIntegrationTest, testMissingService)
    **********/
   ROS_DEBUG("Step 2");
 
-  omit_advertise_func();
+  omit_advertise_func_();
   waitForNode(STOP1_EXECUTOR_NODE_NAME);
 }
 
@@ -118,8 +129,8 @@ TEST(Stop1StartupIntegrationTest, testMissingService)
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "integrationtest_stop1_startup");
-  ros::NodeHandle nh;
+  ros::init(argc, argv, "integrationtest_stop1_service_missing");
+  ros::NodeHandle nh_;
 
   ros::AsyncSpinner spinner{1};
   spinner.start();
