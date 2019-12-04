8568a08c53e40578a92298517ef80c618ee29903
Christian Henkel <post@henkelchristian.de>
2019-12-04 15:41:54 +0100

    gui with override and labels for stati

--- pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
+++ pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
@@ -4,8 +4,12 @@ import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
+#from python_qt_binding.QtCore import 
+from python_qt_binding.QtGui import QIcon, QColor
 from python_qt_binding.QtWidgets import QWidget
 
+OK = "green"
+NOK = "red"
 
 class PilzStatusIndicatorRqt(Plugin):
 
@@ -44,19 +48,41 @@ class PilzStatusIndicatorRqt(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
+        self._widget.labelROS.setStyleSheet("QLabel { background-color: darkgrey }")
+        self._widget.labelPRBT.setStyleSheet("QLabel { background-color: darkgrey }")
+
+        self.set_ROS_status(True)
+        
     def shutdown_plugin(self):
         # TODO unregister all publishers here
         pass
+
 
     def save_settings(self, plugin_settings, instance_settings):
         # TODO save intrinsic configuration, usually using:
         # instance_settings.set_value(k, v)
         pass
 
+
     def restore_settings(self, plugin_settings, instance_settings):
         # TODO restore intrinsic configuration, usually using:
         # v = instance_settings.value(k)
         pass
+
+
+    def set_ROS_status(self, status):
+        self._set_label_status(self._widget.labelROS, status)
+
+
+    def set_PRBT_status(self, status):
+        self._set_label_status(self._widget.labelPRBT, status)
+
+
+    def _set_label_status(self, label, status):
+        if status:
+            label.setStyleSheet("QLabel { background-color: %s }"%OK)
+        else:
+            label.setStyleSheet("QLabel { background-color: %s }"%NOK)
 
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
