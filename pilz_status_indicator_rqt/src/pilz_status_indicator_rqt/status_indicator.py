b39d658380d8be5272332713b9ec4826e98089e9
Christian Henkel <post@henkelchristian.de>
2020-01-14 22:01:20 +0100

    cleaning up code

--- pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
+++ pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
@@ -11,41 +11,28 @@ from prbt_hardware_support.msg import OperationModes
 OK = "green"
 NOK = "red"
 
+TOPIC_OPERATION_MODE = "/prbt/operation_mode"
+TOPIC_DIAGNOSTICS_PRBT = "/prbt/diagnostics/state_prbt"
+TOPIC_DIAGNOSTICS_ROS = "/prbt/diagnostics/state_ros"
+TOPIC_SPEED_OVERRIDE = "/prbt/speed_override"
+
+
 class PilzStatusIndicatorRqt(Plugin):
 
     def __init__(self, context):
         super(PilzStatusIndicatorRqt, self).__init__(context)
-        # Give QObjects reasonable names
         self.setObjectName('PilzStatusIndicatorRqt')
-
-        # Process standalone plugin command-line arguments
-        from argparse import ArgumentParser
-        parser = ArgumentParser()
-        # Add argument(s) to the parser.
-        parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
-        args, unknowns = parser.parse_known_args(context.argv())
-        if not args.quiet:
-            print 'arguments: ', args
-            print 'unknowns: ', unknowns
-
-        # Create QWidget
         self._widget = QWidget()
-        # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('pilz_status_indicator_rqt'), 'resource', 'PilzStatusIndicatorRqt.ui')
-        # Extend the widget with all attributes and children from UI file
+        ui_file = os.path.join(rospkg.RosPack().get_path(
+            'pilz_status_indicator_rqt'), 'resource', 'PilzStatusIndicatorRqt.ui')
         loadUi(ui_file, self._widget)
-        # Give QObjects reasonable names
         self._widget.setObjectName('PilzStatusIndicatorRqtUi')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
-        # tell from pane to pane.
+        # Show _widget.windowTitle on left-top of each plugin (when
+        # it's set in _widget). This is useful when you open multiple
+        # plugins at once.
         if context.serial_number() > 1:
-            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-        # Add widget to the user interface
+            self._widget.setWindowTitle(
+                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         context.add_widget(self._widget)
 
         # checking if widget is loaded correctly from ui file
@@ -55,39 +42,27 @@ class PilzStatusIndicatorRqt(Plugin):
         assert self._widget.barSpeed, "barSpeed must be loaded from ui file"
 
         # prepare ui elements
-        self._widget.labelROS.setStyleSheet("QLabel { background-color: darkgrey }")
-        self._widget.labelPRBT.setStyleSheet("QLabel { background-color: darkgrey }")
+        self._widget.labelROS.setStyleSheet(
+            "QLabel { background-color: darkgrey }")
+        self._widget.labelPRBT.setStyleSheet(
+            "QLabel { background-color: darkgrey }")
         self._widget.labelOM.setScaledContents(True)
 
         # set intial state
         self.set_ROS_status(True)
         self.set_operation_mode(OperationModes.AUTO)
         self.set_speed(.75)
-        
+
+
     def shutdown_plugin(self):
         # TODO unregister all publishers here
         pass
 
-
-    def save_settings(self, plugin_settings, instance_settings):
-        # TODO save intrinsic configuration, usually using:
-        # instance_settings.set_value(k, v)
-        pass
-
-
-    def restore_settings(self, plugin_settings, instance_settings):
-        # TODO restore intrinsic configuration, usually using:
-        # v = instance_settings.value(k)
-        pass
-
-
     def set_ROS_status(self, status):
         self._set_label_status(self._widget.labelROS, status)
 
-
     def set_PRBT_status(self, status):
         self._set_label_status(self._widget.labelPRBT, status)
-
 
     def set_operation_mode(self, operation_mode):
         if operation_mode == OperationModes.AUTO:
@@ -96,23 +71,17 @@ class PilzStatusIndicatorRqt(Plugin):
             icon_name = 't1'
         elif operation_mode == OperationModes.T2:
             icon_name = 't2'
-        icon_path = os.path.join(rospkg.RosPack().get_path('pilz_status_indicator_rqt'), 'resource', icon_name + '.png')
+        icon_path = os.path.join(rospkg.RosPack().get_path(
+            'pilz_status_indicator_rqt'), 'resource', icon_name + '.png')
         pixmap = QPixmap(icon_path)
         self._widget.labelOM.setPixmap(pixmap)
 
-    
     def set_speed(self, val):
         # expecting val = 0...1
         self._widget.barSpeed.setValue(100 * val)
-        
 
     def _set_label_status(self, label, status):
         if status:
-            label.setStyleSheet("QLabel { background-color: %s }"%OK)
+            label.setStyleSheet("QLabel { background-color: %s }" % OK)
         else:
-            label.setStyleSheet("QLabel { background-color: %s }"%NOK)
-
-    #def trigger_configuration(self):
-        # Comment in to signal that the plugin has a way to configure
-        # This will enable a setting button (gear icon) in each dock widget title bar
-        # Usually used to open a modal configuration dialog
+            label.setStyleSheet("QLabel { background-color: %s }" % NOK)
