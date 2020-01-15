59626199a52c19a0d5b4d32c9af88798ede344b1
Christian Henkel <post@henkelchristian.de>
2020-01-15 15:23:10 +0100

    splitting view and ros

--- pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
+++ pilz_status_indicator_rqt/src/pilz_status_indicator_rqt/status_indicator.py
@@ -3,11 +3,14 @@ import rospy
 import rospkg
 
 from qt_gui.plugin import Plugin
+from qt_gui.plugin_context import PluginContext
 from python_qt_binding import loadUi
 from python_qt_binding.QtGui import QIcon, QColor, QPixmap
 from python_qt_binding.QtWidgets import QWidget
 from prbt_hardware_support.msg import OperationModes
 from std_msgs.msg import Bool, Float64
+
+from pilz_status_indicator_rqt import status_indicator_view
 
 GREEN = "green"
 RED = "red"
@@ -19,22 +22,25 @@ TOPIC_SPEED_OVERRIDE = "/prbt/speed_override"
 
 
 class PilzStatusIndicatorRqt(Plugin):
-
     def __init__(self, context):
-        super(PilzStatusIndicatorRqt, self).__init__(context)
-        self.setObjectName('PilzStatusIndicatorRqt')
-        self._widget = QWidget()
+        if context.__class__ == PluginContext:
+            super(PilzStatusIndicatorRqt, self).__init__(context)
+            self.setObjectName('PilzStatusIndicatorRqt')
+            self._widget = QWidget()
+            self.run(self._widget)
+            # Show _widget.windowTitle on left-top of each plugin (when
+            # it's set in _widget). This is useful when you open multiple
+            # plugins at once.
+            if context.serial_number() > 1:
+                self._widget.setWindowTitle(
+                    self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+            context.add_widget(self._widget)
+
+    def run(self, widget):
         ui_file = os.path.join(rospkg.RosPack().get_path(
             'pilz_status_indicator_rqt'), 'resource', 'PilzStatusIndicatorRqt.ui')
         loadUi(ui_file, self._widget)
         self._widget.setObjectName('PilzStatusIndicatorRqtUi')
-        # Show _widget.windowTitle on left-top of each plugin (when
-        # it's set in _widget). This is useful when you open multiple
-        # plugins at once.
-        if context.serial_number() > 1:
-            self._widget.setWindowTitle(
-                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-        context.add_widget(self._widget)
 
         # checking if widget is loaded correctly from ui file
         assert self._widget.labelROS, "ROS label must be loaded from ui file"
@@ -43,58 +49,69 @@ class PilzStatusIndicatorRqt(Plugin):
         assert self._widget.barSpeed, "barSpeed must be loaded from ui file"
 
         # prepare ui elements
-        self._widget.labelROS.setStyleSheet(
-            "QLabel { background-color: darkgrey }")
-        self._widget.labelPRBT.setStyleSheet(
-            "QLabel { background-color: darkgrey }")
         self._widget.labelOM.setScaledContents(True)
 
         # set intial state
-        self.set_ROS_status(Bool(False))
-        self.set_PRBT_status(Bool(False))
-        om_unknown = OperationModes()
-        om_unknown.value = OperationModes.UNKNOWN
-        self.set_operation_mode(om_unknown)
-        self.set_speed(Float64(.5))
+        self._set_ROS_status_view(False)
+        self._set_PRBT_status_view(False)
+        self._set_operation_mode_view(OperationModes.UNKNOWN)
+        self._set_speed_view(.5)
 
         rospy.Subscriber(TOPIC_DIAGNOSTICS_ROS, Bool,
-                         self.set_ROS_status)
+                         self.ros_status_callback)
         rospy.Subscriber(TOPIC_DIAGNOSTICS_PRBT, Bool,
-                         self.set_PRBT_status)
+                         self.prbt_status_callback)
         rospy.Subscriber(TOPIC_OPERATION_MODE, OperationModes,
-                         self.set_operation_mode)
-        rospy.Subscriber(TOPIC_SPEED_OVERRIDE, Float64, self.set_speed)
+                         self.operation_mode_callback)
+        rospy.Subscriber(TOPIC_SPEED_OVERRIDE, Float64, self.speed_callback)
 
     def shutdown_plugin(self):
         pass
 
-    def set_ROS_status(self, msg):
-        status = msg.data
-        self._set_label_status(self._widget.labelROS, status)
 
-    def set_PRBT_status(self, msg):
-        status = msg.data
-        self._set_label_status(self._widget.labelPRBT, status)
+    def prbt_status_callback(self, msg):
+        self._set_PRBT_status_view(msg.data)
 
-    def set_operation_mode(self, operation_mode):
-        rospy.logdebug("set_operation_mode: " + str(operation_mode))
-        if operation_mode.value == OperationModes.AUTO:
+    def ros_status_callback(self, msg):
+        self._set_ROS_status_view(msg.data)
+
+    def _set_ROS_status_view(self, status):
+        self._set_label_status_view(self._widget.labelROS, status)
+
+    def _set_PRBT_status_view(self, status):
+        self._set_label_status_view(self._widget.labelPRBT, status)
+
+    def _set_label_status_view(self, label, status):
+        if status:
+            label.setStyleSheet("QLabel { background-color: %s }" % GREEN)
+        else:
+            label.setStyleSheet("QLabel { background-color: %s }" % RED)
+
+
+    def operation_mode_callback(self, msg):
+        rospy.logdebug("set_operation_mode: " + str(msg))
+        self._set_operation_mode_view(msg.value)
+
+    def _set_operation_mode_view(self, value):
+        if value == OperationModes.AUTO:
             icon_name = 'auto'
-        elif operation_mode.value == OperationModes.T1:
+        elif value == OperationModes.T1:
             icon_name = 't1'
-        elif operation_mode.value == OperationModes.T2:
+        elif value == OperationModes.T2:
             icon_name = 't2'
-        else:  # operation_mode.value == OperationModes.UNKNOWN
+        else:  # value == OperationModes.UNKNOWN
             icon_name = 'unknown'
         icon_path = os.path.join(rospkg.RosPack().get_path(
             'pilz_status_indicator_rqt'), 'resource', icon_name + '.png')
         pixmap = QPixmap(icon_path)
         self._widget.labelOM.setPixmap(pixmap)
 
-    def set_speed(self, msg):
+
+    def speed_callback(self, msg):
         val = msg.data
-        if val == .42:
-            raise Exception("trying to die")
+        self._set_speed_view(val)
+
+    def _set_speed_view(self, val):
         if val > 1 or val < 0:  # expecting val = 0...1
             rospy.logwarn(
                 "expecting speed value between 0 and 1, got {}".format(val))
@@ -103,9 +120,3 @@ class PilzStatusIndicatorRqt(Plugin):
             else:
                 val = 0
         self._widget.barSpeed.setValue(100. * val)
-
-    def _set_label_status(self, label, status):
-        if status:
-            label.setStyleSheet("QLabel { background-color: %s }" % GREEN)
-        else:
-            label.setStyleSheet("QLabel { background-color: %s }" % RED)
