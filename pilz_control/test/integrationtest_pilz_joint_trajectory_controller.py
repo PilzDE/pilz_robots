fa63d31d6a4056d66406bc6ee216301a620a9602
hslusarek <h.slusarek@pilz.de>
2020-03-16 19:39:36 +0100

    Add PJTC integrationtest for Cartesian speed monitoring

--- pilz_control/test/integrationtest_pilz_joint_trajectory_controller.py
+++ pilz_control/test/integrationtest_pilz_joint_trajectory_controller.py
@@ -45,17 +45,17 @@ DEFAULT_GOAL_POSITION = 0.1
 
 controller_ns = rospy.get_param(CONTROLLER_NS_PARAM_NAME)
 
-## Exception class used by MovementObserver
 class ObservationException(Exception):
-  pass
+    """Exception class used by MovementObserver"""
+    pass
 
-## Class that subscribes to the robot mock state to observe the movement.
-#  Enables to run blocking observations until a condition has been fulfilled or a timeout passed
-#
-# \note Current implementation assumes that the robot mock only has one joint.
 class MovementObserver:
+    """Class that subscribes to the robot mock state to observe the movement.
 
-    ## Upon construction subscribes to the state topic.
+    Enables to run blocking observations until a condition has been fulfilled or a timeout passed
+    :note Current implementation assumes that the robot mock only has one joint.
+
+    """
     def __init__(self):
         self._actual_position = None
         self._actual_position_lock = threading.Lock()
@@ -63,40 +63,39 @@ class MovementObserver:
         self._actual_velocity_lock = threading.Lock()
 
         state_topic_name = controller_ns + STATE_TOPIC_NAME
-        self._subscriber = rospy.Subscriber(state_topic_name, JointTrajectoryControllerState, self.controllerStateCallback)
+        self._subscriber = rospy.Subscriber(state_topic_name, JointTrajectoryControllerState, self.controller_state_callback)
 
-    ## Callback use by the state subscription.
-    def controllerStateCallback(self, data):
+    def controller_state_callback(self, data):
         with self._actual_position_lock:
             self._actual_position = data.actual.positions[0]
         with self._actual_velocity_lock:
             self._actual_velocity = data.actual.velocities[0]
 
-    def _checkPositionThreshold(self, position_threshold):
+    def _check_position_threshold(self, position_threshold):
         with self._actual_position_lock:
             return (self._actual_position is None) or (self._actual_position < position_threshold)
 
-    ## Blocking observation until the observed position is above the \p position_threshold
-    #  @throw \class ObservationException position
-    def observeUntilPositionGreater(self, position_threshold, timeout):
+    def observe_until_position_greater(self, position_threshold, timeout):
+        """ Blocking observation until the observed position is above the position_threshold. """
         r = rospy.Rate(SLEEP_RATE_HZ)
         start_loop = rospy.Time.now()
-        while self._checkPositionThreshold(position_threshold):
+        while self._check_position_threshold(position_threshold):
             r.sleep()
             if(timeout < (rospy.Time.now() - start_loop).to_sec()):
               raise ObservationException("Position not above defined threshold within timeout")
 
-    ## Blocking observation that the robot stops within \p timeout seconds, ensures that the deceleration stays below
-    #  \p max_deceleration
-    #  @throw ObservationException
-    #  @note \p max_deceleration is defined positive
-    def observeStop(self, max_deceleration = MAX_DECELERATION, timeout = STOP_DURATION_UPPER_BOUND_S):
+    def observe_stop(self, max_deceleration = MAX_DECELERATION, timeout = STOP_DURATION_UPPER_BOUND_S):
+        """ Blocking observation that the robot stops within timeout seconds,
+            ensures that the deceleration stays below max_deceleration.
+
+        :param max_deceleration: Max allowed deceleration. The deceleration has to be positive.
+        :param timeout: Time in which robot has to stop.
+        """
         r = rospy.Rate(SLEEP_RATE_HZ)
         start_stop = rospy.Time.now()
 
         # Main observation loop, not infinite due to timeout
         while True:
-
             # Obtain local of actual velocity
             actual_velocity = None
             with self._actual_position_lock:
@@ -104,7 +103,7 @@ class MovementObserver:
 
             # Timeout check
             stop_duration = (rospy.Time.now() - start_stop).to_sec()
-            if(timeout < stop_duration):
+            if timeout < stop_duration:
                 raise ObservationException('Stop lasted too long: ' + str(stop_duration) + ' seconds.')
 
             # Noting to be done if no actual_velocity was observed
@@ -121,48 +120,47 @@ class MovementObserver:
 
             # Check for abrupt stop
             with self._actual_position_lock:
-                if(max_deceleration < abs(self._actual_velocity - old_velocity) * SLEEP_RATE_HZ):
+                if max_deceleration < abs(self._actual_velocity - old_velocity) * SLEEP_RATE_HZ:
                     raise ObservationException("Abrupt stop detected!")
 
 
-## Essentially a wrapper around a SimpleActionClient for dispatching simple trajectories.
 class TrajectoryDispatcher:
+    """A wrapper around the SimpleActionClient for dispatching trajectories."""
+    def __init__(self):
+        action_name = controller_ns + ACTION_NAME
+        self._client = actionlib.SimpleActionClient(action_name, FollowJointTrajectoryAction)
 
-  def __init__(self):
-      action_name = controller_ns + ACTION_NAME
-      self._client = actionlib.SimpleActionClient(action_name, FollowJointTrajectoryAction)
+        timeout = rospy.Duration(WAIT_FOR_SERVICE_TIMEOUT_S)
+        self._client.wait_for_server(timeout)
 
-      timeout = rospy.Duration(WAIT_FOR_SERVICE_TIMEOUT_S)
-      self._client.wait_for_server(timeout)
+    def dispatch_trajectory(self, goal_position, time_from_start):
+        """Sends a simple JointTrajectory to the action client.
 
-  ## Sends a simple JointTrajectory to the action client
-  #  @param goal_position The only position in the send trajectory
-  #  @param time_from_start The time of the only position to be achieved (Starting at 0)
-  def dispatchTrajectory(self, goal_position, time_from_start):
-      point = JointTrajectoryPoint()
-      point.positions = [goal_position]
-      point.time_from_start = rospy.Duration(time_from_start)
+        :param goal_position: The only position in the send trajectory
+        :param time_from_start: The time of the only position to be achieved (Starting at 0)
+        """
+        point = JointTrajectoryPoint()
+        point.positions = [goal_position]
+        point.time_from_start = rospy.Duration(time_from_start)
 
-      goal = FollowJointTrajectoryGoal()
-      goal.trajectory.header.stamp = rospy.Time.now()
-      goal.trajectory.points = [point]
-      goal.trajectory.joint_names = JOINT_NAMES
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory.header.stamp = rospy.Time.now()
+        goal.trajectory.points = [point]
+        goal.trajectory.joint_names = JOINT_NAMES
 
-      self._client.send_goal(goal)
+        self._client.send_goal(goal)
 
-  ## Wait until the result of the trajectory execution is received
-  def waitForResult(self):
-      self._client.wait_for_result()
-      return self._client.get_result()
+    def wait_for_result(self):
+        """Wait until the result of the trajectory execution is received."""
+        self._client.wait_for_result()
+        return self._client.get_result()
 
-  ## Get the state of the last send trajectory
-  #  @return GoalStatus
-  #  see <a href="http://docs.ros.org/melodic/api/actionlib_msgs/html/msg/GoalStatus.html">
-  #       http://docs.ros.org/melodic/api/actionlib_msgs/html/msg/GoalStatus.html</a>
-  #  @note Check result with e.g.
-  #        \code self.assertEqual(GoalStatus.PREEMPTED, dispatcher.getLastState(), "Unexpected result code") \endcode
-  def getLastState(self):
-      return self._client.get_state()
+    def get_last_state(self):
+        """Get the state of the last send trajectory
+
+        :return: see http://docs.ros.org/melodic/api/actionlib_msgs/html/msg/GoalStatus.html
+        """
+        return self._client.get_state()
 
 class SetMonitoredCartesianSpeed:
     def __init__(self):
@@ -176,121 +174,142 @@ class SetMonitoredCartesianSpeed:
         resp = self._set_catesian_speed_srv(req)
         return resp.success
 
-    def turnSpeedMonitoringOff(self):
+    def turn_off_speed_monitoring(self):
         return self._call(False)
 
-    def turnSpeedMonitoringOn(self):
+    def turn_on_speed_monitoring(self):
         return self._call(True)
 
 
-## Wrapper for the service querying if the controller is executing
 class IsExecutingServiceWrapper:
-  def __init__(self):
+    """Wrapper for the service querying if the controller is executing."""
+    def __init__(self):
+        is_executing_service_name = controller_ns + IS_EXECUTING_SERVICE_NAME
+        rospy.wait_for_service(is_executing_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
+        self._is_executing_srv = rospy.ServiceProxy(is_executing_service_name, Trigger)
 
-      is_executing_service_name = controller_ns + IS_EXECUTING_SERVICE_NAME
-      rospy.wait_for_service(is_executing_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
-      self._is_executing_srv = rospy.ServiceProxy(is_executing_service_name, Trigger)
+    def call(self):
+        req = TriggerRequest()
+        resp = self._is_executing_srv(req)
 
-  def call(self):
-      req = TriggerRequest()
-      resp = self._is_executing_srv(req)
+        return resp.success
 
-      return resp.success
-
-
-## Abstraction around the service call for switch the controller between DEFAULT and HOLDING mode
 class StopServiceWrapper:
-  def __init__(self):
+    """Abstraction around the service call to switch the controller between DEFAULT and HOLDING mode."""
+    def __init__(self):
+        hold_service_name = controller_ns + HOLD_SERVICE_NAME
+        rospy.wait_for_service(hold_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
+        self._hold_srv = rospy.ServiceProxy(hold_service_name, Trigger)
 
-      hold_service_name = controller_ns + HOLD_SERVICE_NAME
-      rospy.wait_for_service(hold_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
-      self._hold_srv = rospy.ServiceProxy(hold_service_name, Trigger)
+        unhold_service_name = controller_ns + UNHOLD_SERVICE_NAME
+        rospy.wait_for_service(unhold_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
+        self._unhold_srv = rospy.ServiceProxy(unhold_service_name, Trigger)
 
-      unhold_service_name = controller_ns + UNHOLD_SERVICE_NAME
-      rospy.wait_for_service(unhold_service_name, WAIT_FOR_SERVICE_TIMEOUT_S)
-      self._unhold_srv = rospy.ServiceProxy(unhold_service_name, Trigger)
+    def request_default_mode(self):
+        """Switch into DEFAULT mode by sending the respective request.
 
-  ## Switch into DEFAULT mode by sending the respective request
-  #  @return True if service request was handled successful, False otherwise
-  def requestDefaultMode(self):
-      req = TriggerRequest()
-      resp = self._unhold_srv(req)
+        :return: True if service request was handled successful, False otherwise.
+        """
+        req = TriggerRequest()
+        resp = self._unhold_srv(req)
 
-      return resp.success
+        return resp.success
 
-  ## Switch into HOLDING mode by sending the respective request
-  #  @return True if service request was handled successful, False otherwise
-  def requestHoldingMode(self):
-      req = TriggerRequest()
-      resp = self._hold_srv(req)
+    def request_holding_mode(self):
+        """Switch into HOLDING mode by sending the respective request.
 
-      return resp.success
+        :return: True if service request was handled successful, False otherwise
+        """
+        req = TriggerRequest()
+        resp = self._hold_srv(req)
 
-## Test the influence of the HOLD mode on joint trajectory goals. For this a small robot-mock is started and the
-#  controller is spawned via the test launch file.
-class IntegrationtestPilzJointTrajectoryController(unittest.TestCase):
+        return resp.success
 
-    ## Test Sequence:
-    #        1. Set up service proxy and action client.
-    #        2. Send goal to controller action server.
-    #        3. Request switch to DEFAULT mode via service.
-    #        4. Send goal to controller action server.
-    #        5. Send new goal to controller action server and switch to HOLDING during execution.
-    #
-    #    Expected Results:
-    #        1. Wait for service and action server are successful.
-    #        2. Goal is not executed.
-    #        3. Service call is successful.
-    #        4. Goal is executed.
-    #        5. Goal execution is stopped. The robot is led into a hold position, but not stopped abruptly.
-    def test_hold_unhold_functionality(self):
+class TestPilzJointTrajectoryController(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestPilzJointTrajectoryController, self).__init__(*args, **kwargs)
 
-        rospy.loginfo("1. Set up hold service and trajectory dispatcher.")
-        action_name = controller_ns + ACTION_NAME
+        self._trajectory_dispatcher = TrajectoryDispatcher()
+        self._monitored_cartesian_speed_srv = SetMonitoredCartesianSpeed()
+        self._hold_srv = StopServiceWrapper()
 
-        hold_srv = StopServiceWrapper()
-        trajectory_dispatcher = TrajectoryDispatcher()
+    def setUp(self):
+        self.assertTrue(self._turn_off_speed_monitoring(), 'Could not turn off speed monitoring')
+
+    def _move_to(self, position, duration, expected_error_code=FollowJointTrajectoryResult.SUCCESSFUL):
+        self._start_motion(position=position, duration=duration)
+        return self._wait_for_motion_result(expected_error_code)
+
+    def _start_motion(self, position, duration):
+        self._trajectory_dispatcher.dispatch_trajectory(goal_position=position, time_from_start=duration)
+
+    def _wait_for_motion_result(self, expected_error_code=FollowJointTrajectoryResult.SUCCESSFUL):
+        result = self._trajectory_dispatcher.wait_for_result()
+        return result.error_code == expected_error_code
+
+    def _turn_off_speed_monitoring(self):
+        return self._monitored_cartesian_speed_srv.turn_off_speed_monitoring()
+
+    def _turn_on_speed_monitoring(self):
+        return self._monitored_cartesian_speed_srv.turn_on_speed_monitoring()
+
+    def test_hold_at_startup(self):
+        """Tests that 'hold' mode is active at startup of controller."""
+
+        self.assertTrue(self._move_to(position=DEFAULT_GOAL_POSITION, duration=1,
+                                      expected_error_code=FollowJointTrajectoryResult.INVALID_GOAL),
+                        "Motion did not fail although controller should be in 'hold' mode at startup")
+
+        self.assertTrue(self._hold_srv.request_default_mode(), 'Switch to default mode failed')
+
+        self.assertTrue(self._move_to(position=DEFAULT_GOAL_POSITION, duration=0.5),
+                        "Motion failed although controller should be in 'unhold' mode")
+
+    def test_hold_during_motion(self):
+        """Activate hold mode while robot is moving and evaluate executed stop."""
         is_executing_srv = IsExecutingServiceWrapper()
-        monitored_cartesian_speed_srv = SetMonitoredCartesianSpeed()
 
-        self.assertTrue(monitored_cartesian_speed_srv.turnSpeedMonitoringOff(), 'Could not turn off speed monitoring')
-
-        rospy.loginfo("2. Send goal to controller action server. Default startup state should be holding!!!!")
-        trajectory_dispatcher.dispatchTrajectory(goal_position = DEFAULT_GOAL_POSITION, time_from_start = 1)
-        result = trajectory_dispatcher.waitForResult()
-        self.assertEqual(FollowJointTrajectoryResult.INVALID_GOAL, result.error_code,
-                         'Unexpected error code in result of ' + action_name + ' action: ' + str(result.error_code))
-
-        rospy.loginfo("3. Request switch to DEFAULT mode via service.")
-        self.assertTrue(hold_srv.requestDefaultMode(), 'Switch to Mode DEFAULT failed.')
-
-        rospy.loginfo("4. Send goal to controller action server.")
-        trajectory_dispatcher.dispatchTrajectory(goal_position = DEFAULT_GOAL_POSITION, time_from_start = 0.5)
-        result = trajectory_dispatcher.waitForResult()
-        self.assertEqual(FollowJointTrajectoryResult.SUCCESSFUL, result.error_code, 'Action goal was not successful.')
-
-        rospy.loginfo("5. Send new goal to controller action server and switch to HOLDING during execution.")
-        trajectory_dispatcher.dispatchTrajectory(goal_position = 0.2, time_from_start = 5)
+        self._start_motion(position = 0.2, duration = 5)
 
         # Wait for movement to commence
-        MovementObserver().observeUntilPositionGreater(0.11, 3)
-        self.assertTrue(is_executing_srv.call(), 'is_executing service returned false.')
-        # Hold robot during movement
-        rospy.loginfo("  - Robot is moving -> Switch into HOLDING mode")
-        self.assertTrue(hold_srv.requestHoldingMode(), 'Switch to Mode HOLDING failed.')
+        MovementObserver().observe_until_position_greater(0.11, 3)
+        self.assertTrue(is_executing_srv.call(), "Controller is not executing")
+        rospy.loginfo("Robot is moving -> Switch into HOLDING mode")
+        self.assertTrue(self._hold_srv.request_holding_mode(), 'Switch to Mode HOLDING failed.')
 
         # Make sure robot stops (not abruptly)
-        MovementObserver().observeStop()
-        rospy.loginfo("  - Stop of robot observed!")
+        MovementObserver().observe_stop()
+        rospy.loginfo("Stop of robot observed!")
 
-        result = trajectory_dispatcher.waitForResult()
-        self.assertEqual(FollowJointTrajectoryResult.SUCCESSFUL, result.error_code, 'Action goal was not successful.')
+        self.assertTrue(self._wait_for_motion_result(), "Motion failed")
 
-        # Make sure goal was preempted
-        self.assertEqual(GoalStatus.PREEMPTED, trajectory_dispatcher.getLastState(), 'Action goal was not preempted.')
+        self.assertEqual(GoalStatus.PREEMPTED, self._trajectory_dispatcher.get_last_state(), 'Goal was not preempted.')
+
+    def test_speed_monitoring(self):
+        """Tests if controller detects Cartesian speed limit violation and switches to hold mode."""
+        far_away_position = 0.5
+
+        self.assertTrue(self._turn_on_speed_monitoring(), "Could not turn on speed monitoring")
+
+        self.assertTrue(self._hold_srv.request_default_mode(), "Switch to default mode failed")
+
+        self.assertTrue(self._move_to(position=DEFAULT_GOAL_POSITION, duration=0.5),
+                        "Motion to default position failed")
+
+        self.assertTrue(self._move_to(position=far_away_position, duration=0.1),
+                        "Cartesian speed monitor did not detect speed limit violation")
+
+        self.assertTrue(self._move_to(position=far_away_position, duration=0.5,
+                                 expected_error_code=FollowJointTrajectoryResult.INVALID_GOAL),
+                         "Controller did not block motion execution although controller should be in 'hold' mode")
+
+        self.assertTrue(self._hold_srv.request_default_mode(), "Switch to 'unhold' mode failed")
+
+        self.assertTrue(self._move_to(position=far_away_position, duration=0.5),
+                        "Controller did not allow motion execution, although controller should be in 'unhold' mode")
+
 
 if __name__ == '__main__':
     import rostest
     rospy.init_node('integrationtest_pilz_joint_trajectory_controller')
-    rostest.rosrun(PACKAGE_NAME, 'integrationtest_pilz_joint_trajectory_controller',
-                   IntegrationtestPilzJointTrajectoryController)
+    rostest.rosrun(PACKAGE_NAME, 'integrationtest_pilz_joint_trajectory_controller', TestPilzJointTrajectoryController)
